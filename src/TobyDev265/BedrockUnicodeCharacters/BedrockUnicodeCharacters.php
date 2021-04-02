76d684708a98c65ef635963eb10ffa29cfd87b69
TobyDev265 <tobyd265@outlook.com>
2021-04-02 15:41:20 +0700

    Upload files

---
+++ src/TobyDev265/BedrockUnicodeCharacters/BedrockUnicodeCharacters.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TobyDev265\BedrockUnicodeCharacters;
+
+class BedrockUnicodeCharacters {
+    const FORWARD_ARROW_BUTTON = "";
+    const LEFT_ARROW_BUTTON = "";
+    const BACKWARDS_ARROW_BUTTON = "";
+    const RIGHT_ARROW_BUTTON = "";
+    const JUMP_BUTTON = "";
+    const CROUCH_BUTTON = "";
+    const FLY_UP_BUTTON = "";
+    const FLY_DOWN_BUTTON = "";
+    const CRAFTABLE_BUTTON_ON = "";
+    const CRAFTABLE_BUTTON_OFF = "";
+    const FOOD_ICON = "";
+    const ARMOR_ICON = "";
+    const MINECOIN = "";
+    const CODE_BUILDER_BUTTON = "";
+    const IMMERSIVE_READER_BUTTON = "";
+    const TOKEN = "";
+}
